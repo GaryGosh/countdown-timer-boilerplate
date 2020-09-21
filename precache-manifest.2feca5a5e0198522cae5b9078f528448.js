95ce6eecc9b1977114f97e017813ba7be2154202
GaryGosh <clichethe1@yahoo.com>
2020-09-21 23:21:06 +0530

    Updates

---
+++ precache-manifest.2feca5a5e0198522cae5b9078f528448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93b041af42c3f450ee74316b3144776",
+    "url": "/countdown-timer-boilerplate/index.html"
+  },
+  {
+    "revision": "b66c3b6c0686ac50751a",
+    "url": "/countdown-timer-boilerplate/static/css/main.8f332ea3.chunk.css"
+  },
+  {
+    "revision": "e4d7108380b3255135f2",
+    "url": "/countdown-timer-boilerplate/static/js/2.74ed5750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-timer-boilerplate/static/js/2.74ed5750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66c3b6c0686ac50751a",
+    "url": "/countdown-timer-boilerplate/static/js/main.b5e8bef7.chunk.js"
+  },
+  {
+    "revision": "3b9a2e4c3d48c28dd7d1",
+    "url": "/countdown-timer-boilerplate/static/js/runtime-main.6ef87fe9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/countdown-timer-boilerplate/static/media/logo.ee7cd8ed.svg"
+  }
+]);
